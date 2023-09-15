ecd113436d9b86e37e27f1cbd35be30893a41e9c
lanakuge <maulananizhar27@gmail.com>
2023-09-15 18:33:22 +0700

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Nizhar Maulana",
+  description: '"A software developer. Born and live in Jakarta, Indonesia.',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { Button } from "@/components/button";
+import * as React from "react";
+
+export default function Home() {
+  const [num, setNum] = React.useState(0);
+
+  return (
+    <div className="dark:bg-zinc-950 bg-white">
+      <nav className="container">
+        <p>woke</p>
+      </nav>
+    </div>
+  );
+}
