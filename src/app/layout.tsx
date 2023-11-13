3cf3bfcee957bbfea92ccdf5c86883734bef7ab7
Nizhar Maulana <maulananizhar27@gmail.com>
2023-11-13 18:29:23 +0700

    fix : disable system theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
         </ThemeProvider>
       </body>
