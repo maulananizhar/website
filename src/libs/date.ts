f4eb36b03a0f6c5ba1e7192c0685bb32f683b136
Nizhar Maulana <maulananizhar27@gmail.com>
2023-11-19 10:03:55 +0700

    new : create function date converter

---
+++ src/libs/date.ts
@@ -0,0 +1,26 @@
+export function numberToDateString(timestamp: number) {
+  const date = new Date(timestamp);
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  const formattedDate = `${day} ${month} ${year}`;
+
+  return formattedDate;
+}
