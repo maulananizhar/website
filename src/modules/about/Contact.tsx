c7fe49b6f87fa2f312f316c1b0f56c98cbbc7885
Nizhar Maulana <maulananizhar27@gmail.com>
2023-11-21 08:44:27 +0700

    new(modules) : add Contact function component

---
+++ src/modules/about/Contact.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+
+export default function Contact() {
+  return (
+    <>
+      <div className="container px-4 md:px-36 lg:px-48 my-16">
+        <p className="font-mono text-cyan-700 dark:text-cyan-500 mb-4">
+          Get In Touch
+        </p>
+        <p className="mb-1">
+          You can reach me out via email at:{" "}
+          <Link
+            className="hover:text-cyan-700 hover:dark:text-cyan-500 duration-500 underline"
+            href="mailto:maulananizhar27@gmail.com"
+            target="_blank">
+            maulananizhar27@gmail.com
+          </Link>{" "}
+          or via socials below:
+        </p>
+        <ul className="list-disc mx-6">
+          <li>
+            GitHub -{" "}
+            <Link
+              className="hover:text-cyan-700 hover:dark:text-cyan-500 duration-500 underline"
+              href="https://github.com/lanakuge"
+              target="_blank">
+              https://github.com/lanakuge
+            </Link>
+          </li>
+          <li>
+            Instagram -{" "}
+            <Link
+              className="hover:text-cyan-700 hover:dark:text-cyan-500 duration-500 underline"
+              href="https://instagram.com/maulananizhar_"
+              target="_blank">
+              https://instagram.com/maulananizhar_
+            </Link>
+          </li>
+          <li>
+            Twitter -{" "}
+            <Link
+              className="hover:text-cyan-700 hover:dark:text-cyan-500 duration-500 underline"
+              href="https://twitter.com/lanaberg_"
+              target="_blank">
+              https://twitter.com/lanaberg_
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
