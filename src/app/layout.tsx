cd1146f63cf8c3612d8f3fb65d2ed653d74e9d6c
Nizhar Maulana <maulananizhar27@gmail.com>
2023-12-04 10:47:57 +0700

    fix : initialize vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
--- src/app/uses/layout.tsx
+++ src/app/uses/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <ThemeProvider attribute="class" defaultTheme="dark">
       {children}
+      <Analytics />
     </ThemeProvider>
   );
 }
